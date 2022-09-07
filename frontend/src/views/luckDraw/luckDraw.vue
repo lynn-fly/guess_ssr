0a1d0a755118ca822708cef5d526a2f26740f885
dongzhan71 <dongz@shinetechchina.com>
2022-09-07 11:28:23 +0800

    no message

--- frontend/src/views/luckDraw/luckDraw.vue
+++ frontend/src/views/luckDraw/luckDraw.vue
@@ -60,23 +60,23 @@ export default {
       imgData: [
         {
           icon: require("@/assets/luckDraw/111.png"),
-          name: "户外桌椅-灰",
+          name: "AVATR户外桌椅-灰",
         },
         {
           icon: require("@/assets/luckDraw/222.png"),
-          name: "阿峰塔定制保温杯",
+          name: "AVATR定制保温杯",
         },
         {
           icon: require("@/assets/luckDraw/333.png"),
-          name: "城市画展系列T恤衫-XL ",
+          name: "AVATR城市画展系列T恤衫",
         },
         {
           icon: require("@/assets/luckDraw/444.png"),
-          name: "户外折叠整理箱-灰",
+          name: "AVATR户外折叠整理箱-灰",
         },
         {
           icon: require("@/assets/luckDraw/555.png"),
-          name: "户外超声波防潮野餐地垫-灰",
+          name: "AVATR户外超声波防潮野餐地垫-灰",
         },
         {
           icon: require("@/assets/luckDraw/666.png"),
@@ -88,11 +88,11 @@ export default {
         },
         {
           icon: require("@/assets/luckDraw/888.png"),
-          name: "杜邦电脑包",
+          name: "AVATR杜邦电脑包",
         },
         {
           icon: require("@/assets/luckDraw/e1.png"),
-          name: "E值",
+          name: "AVATR E值",
         },
       ],
       imgDataRseult: [
